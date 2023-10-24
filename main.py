fa781ad09304dd0ce475002900ed02389b5f68be
d-wack <dotwack@pm.me>
2023-10-25 01:41:11 +0300

    Cleaned up Code

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ logging.basicConfig(filename='saleforce.log', level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger = logging.getLogger("logger")
 
+DEV = True
+DAYS_OF_LOGS_TO_PULL = 30
+
 
 def get_secret():
     secret_name = "prod/salesforce_event_api"
@@ -37,9 +40,6 @@ def get_secret():
     return json.loads(get_secret_value_response['SecretString'])
 
 
-local_timezone = pytz.timezone('America/New_York')
-
-
 def get_username(sf, user_id):
     results = sf.query_all_iter(
         "SELECT Username FROM User WHERE Id='{0}'".format(user_id))
@@ -54,16 +54,17 @@ def get_last_login_time(organization):
             lines = log.readlines()[-1]
             return json.loads(lines)["LoginTime"]
     else:
-        return (datetime.now(timezone.utc) - timedelta(minutes=30)).strftime('%Y-%m-%dT%H:%M:%S.%f%z')
+        return (datetime.now(timezone.utc) - timedelta(days=DAYS_OF_LOGS_TO_PULL)).strftime('%Y-%m-%dT%H:%M:%S.%f%z')
 
 
-def get_sf_logs(org, secrets):
+def get_sf_logs(organization, credentials):
     sf = None
     if org == 'fountainlife':
         logger.info("Pulling Fountainlife Salesforce Login History Logs")
         try:
-            sf = Salesforce(username=secrets["FL_USERNAME"], password=secrets["PASSWORD"],
-                            security_token=secrets["FL_TOKEN"])
+            sf = Salesforce(username=credentials["FL_USERNAME"], password=credentials["PASSWORD"],
+                            security_token=credentials["FL_TOKEN"])
+            logger.info("Successfully Authenticated with Fountainlife Salesforce")
         except SalesforceAuthenticationFailed:
             logger.critical("Authentication Failed - Fountainlife")
             exit()
@@ -72,15 +73,18 @@ def get_sf_logs(org, secrets):
     else:
         logger.info("Pulling FountainHealth Salesforce Login History Logs")
         try:
-            sf = Salesforce(username=secrets["FH_USERNAME"], password=secrets["PASSWORD"], security_token=secrets["FH_TOKEN"])
+            sf = Salesforce(username=credentials["FH_USERNAME"], password=credentials["PASSWORD"],
+                            security_token=credentials["FH_TOKEN"])
+            logger.info("Successfully Authenticated with FountainHealth Salesforce")
         except SalesforceAuthenticationFailed:
             logger.critical("Authentication Failed - FountainHealth")
             exit()
         except SalesforceGeneralError as e:
             logger.error(e.message)
+    if DEV:
+        print(type(get_last_login_time(organization=org)))
+        print(get_last_login_time(organization=org))
 
-    print(type(get_last_login_time(organization=org)))
-    print(get_last_login_time(organization=org))
     query = (f"SELECT\n"
              f"LoginTime,UserID,Status,SourceIP,Platform,Application,\n"
              f"LoginGeoId,CountryIso\n"
@@ -95,33 +99,34 @@ def get_sf_logs(org, secrets):
                 locations = sf.query(
                     f"SELECT Country,City, Latitude, Longitude FROM LoginGeo WHERE Id='{result.get('LoginGeoId')}'")
                 records = locations.get('records')[0]
-                Latitude = records.get('Latitude')
-                Longitude = records.get('Longitude')
-                City = records.get('City')
+                latitude = records.get('Latitude')
+                longitude = records.get('Longitude')
+                city = records.get('City')
             else:
-                Latitude = None
-                Longitude = None
-                City = None
+                latitude = None
+                longitude = None
+                city = None
             login = {
                 "LoginTime": datetime.strptime(result.get('LoginTime'), '%Y-%m-%dT%H:%M:%S.%f%z').isoformat(),
                 "LoginType": result.get('LoginType'),
                 "Username": get_username(sf=sf, user_id=result.get('UserId')),
-                "Location": {"Longitude": Longitude, "Latitude": Latitude, "City": City,
+                "Location": {"Longitude": longitude, "Latitude": latitude, "City": city,
                              "Country": result.get('CountryIso')},
                 "Status": result.get('Status'),
                 "LoginUrl": result.get('LoginUrl'),
                 "Application": result.get('Application'),
                 "Platform": result.get('Platform'),
-                "Organization": org
+                "Organization": organization
             }
-            print(login)
-            with open("{}_sf_login_history.log".format(org), "a", encoding='utf-8') as logs:
+            if DEV:
+                print(login)
+            with open("{}_sf_login_history.log".format(organization), "a", encoding='utf-8') as logs:
                 logs.write(json.dumps(login) + '\n')
     else:
         logger.info("No records found!!")
 
 
 orgs = ['fountainlife', 'fountainhealth']
-secrets = get_secret()
+creds = get_secret()
 for org in orgs:
-    get_sf_logs(org=org, secrets=secrets)
+    get_sf_logs(organization=org, credentials=creds)
